1e9b0eef3675926b03cd5f8c4814fc307ff6d496
DESKTOP-H468MRI\Moucy Cat <aierzhiguang2@gmail.com>
2018-08-27 16:33:26 +0800

    練習課本裡教學的繼承方式

---
+++ ConsoleApplication1/Fruit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Fruit
+    {
+        public static double Price { get; set; }
+        public virtual void FruitA()
+        {
+            return;
+        }
+    }
+
+    class FruitPics : Fruit
+    {
+        public override void FruitA()
+        {
+            Console.WriteLine("how many pics?");
+            int pics = int.Parse(Console.ReadLine());
+            Console.WriteLine("how much each cost?");
+            double cost = double.Parse(Console.ReadLine());
+            Fruit.Price = (double)pics * cost;
+        }
+    }
+
+    class FruitKG : Fruit
+    {
+        public override void FruitA()
+        {
+            Console.WriteLine("how much kg?");
+            double kg = double.Parse(Console.ReadLine());
+            Console.WriteLine("how much per kg cost?");
+            double cost = double.Parse(Console.ReadLine());
+            Fruit.Price = kg * cost;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("wat type? (1:pic 2:kg 3:leave): ");
+                int choose = int.Parse(Console.ReadLine());
+                if (choose == 1)
+                {
+                    FruitPics pic = new FruitPics();
+                    pic.FruitA();
+                }
+                else if (choose == 2)
+                {
+                    FruitKG kg = new FruitKG();
+                    kg.FruitA();
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine("you must pay " + Fruit.Price);
+                Console.ReadLine();
+            }
+        }
+    }
+}
